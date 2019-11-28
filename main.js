8f62d8cb0cc11a40eb53d2523e694f240581cd12
Christian Krug <christian.krug@sti-group.com>
2019-11-28 21:09:08 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+function preload(){
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(255,0,0);
+
+
+}
+
+function mouseMoved() {
+
+}
+
+function mouseClicked() {
+
+}
