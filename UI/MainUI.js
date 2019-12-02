1ac0e954ee6a92bd866b6004bad7444d06845878
Maximilian B <M.blinzler@outlook.de>
2019-12-02 17:24:12 +0100

    Anpasungen an breite

--- UI/MainUI.js
+++ UI/MainUI.js
@@ -13,7 +13,7 @@ class MainUI {
     var textHeight = 19;
     fill(60);
     stroke(255);
-    rect(0, 0, windowWidth - 51, ra);
+    rect(0, 0, windowWidth - 21, ra);
     fill(255);
     textSize(17);
     noStroke();
